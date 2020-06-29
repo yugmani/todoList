5c09d1de0b7b3811d9272109d18748ec7e47d4fa
Himalaya <yugmani@yahoo.com>
2020-06-29 11:56:16 -0700

    finalize

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
+var todos = [];
 
+init();
 
 // Array of todos
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+// var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
 renderTodo();
 // function to render the each item of todos array.
+
+
 function renderTodo(){
     todoList.textContent = "";
     todoCountSpan.textContent = todos.length;
@@ -23,11 +27,36 @@ function renderTodo(){
         //creating btn element
         var btnEl = document.createElement("button");
         btnEl.textContent = "Compelete";
-        btnEl.setAttribute('data-index', i);
+        liEl.setAttribute('data-index', i);
         liEl.appendChild(btnEl);
+        // console.log(btnEl);
     }
     
 }
+
+
+function init() {
+    // Write code here to check if there are todos in localStorage
+    // If so, parse the value from localStorage and assign it to the todos variable
+    var todoLocal = JSON.parse(localStorage.getItem("todos"));
+    if (todoLocal) {
+        
+        todos = todoLocal;
+    } 
+    else {
+        console.log("Not available in local storage");
+    }
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodo();
+  }
+  
+  function storeTodos() {
+    // Add code here to stringify the todos array and save it to the "todos" key in localStorage
+    localStorage.setItem("todos",  JSON.stringify(todos));
+  }
+  
 
 // Eventlistner to add new todo in todo list. 
 todoForm.addEventListener("submit", function(e){
@@ -36,21 +65,28 @@ todoForm.addEventListener("submit", function(e){
         if (newTodo !== "") {
             todos.push(newTodo);
             todoInput.value = ("");
-            renderTodo();
+            
         }
         else return;  
+    storeTodos();
+    renderTodo();
 })
 
 
 // Eventlistenr to remove a todo from the list.
 todoList.addEventListener("click", function(event){
     var element = event.target;
-
+  
     if (element.matches("button") === true){
+        //getting data-index of the li parent-element of corresponding button clicked.
         var index = element.parentElement.getAttribute("data-index");
 
+    // removing element[index] from Todos array. 
         todos.splice(index, 1);
-        renderTodo();
-    }
 
+    //update renderTodo and storeTodos.
+        renderTodo();
+        storeTodos();   
+    }
+    
 })
